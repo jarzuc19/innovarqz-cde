2ea2d228aa60902f5e8005aeb25b7bffac2ae4ef
jarzuc19 <jarzuc121988@gmail.com>
2026-09-01 18:05:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // ==============================================================================
 const SUPABASE_URL = "https://bjlqtzrcrofpqlmyvoob.supabase.co";
 const SUPABASE_KEY = "sb_publishable_htPtQvL-1wrLfu7ACHBg1w_epAZsu1E";
-const WEBHOOK_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycbxL-gzS5e3FSjePYKzoML2NHok7-2YUKr4x8s0bbZtTxdquFTRDV1TzakVPBTAnmVkZNA/exec";
+const WEBHOOK_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycbz1Y_F6socjfP_DN_PJ7IE5k0iBs6XmnYPaSHDun3p_Bn0y6r6DtD-EZLtTRjUBBwMqWg/exec";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
